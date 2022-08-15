c8a83087f95df9bcb4ce8085228a96d7f230b734
Leandro Custódio <leancp@gmail.com>
2022-08-15 14:51:04 -0300

    Aula CRUD update

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from .views import persons_list
 from .views import person_new
+from .views import persons_update
 
 urlpatterns = [
     path('list/', persons_list, name='person_list'),
     path('new/', person_new, name='person_new'),
+    path('update/<int:id>', persons_update, name='persons_update' )
 ]
